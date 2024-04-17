e5b5c303f11788ba84a6d1fb7e7f726e59d9f668
bsquare63 <bsquare@umich.edu>
2024-04-17 10:11:54 -0400

    Added chi

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bsquare63/lenslocked
 
 go 1.21.0
+
+require github.com/go-chi/chi/v5 v5.0.12 // indirect
--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:bsquare@umich.edu\">bsquare@umich.edu</a>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+	})
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
