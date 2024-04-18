f4ff42924e4c3dd8cf84a65a86d70586451cbb78
bsquare63 <bsquare@umich.edu>
2024-04-18 08:51:19 -0400

    Fix undefined

--- views/template.go
+++ views/template.go
@@ -12,7 +12,7 @@ type Template struct {
 
 func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	err = t.HTMLTpl.Execute(w, nil)
+	err := t.HTMLTpl.Execute(w, nil)
 	if err != nil {
 		log.Printf("Error executing template: %v", err)
 		http.Error(w, "There was an internal error executing the template.", http.StatusInternalServerError)
